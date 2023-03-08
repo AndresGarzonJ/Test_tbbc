ce007bda402d42e71eb3635d9d9bdba542799817
AndresGarzonJ <sebasgarzonj@gmail.com>
2023-03-08 05:48:39 -0500

    DigitalOcean_postgresql_Linux

--- test_tbbc/test_tbbc/settings.py
+++ test_tbbc/test_tbbc/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'test_tbbc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'tbbc',
         'USER': 'admin_tbbc',
         'PASSWORD': '123456',
